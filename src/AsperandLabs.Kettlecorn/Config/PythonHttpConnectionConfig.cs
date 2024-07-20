96317ab6f22f9a5345045ebbd4102dfd2120875a
asperand <jamesdavis0901@gmail.com>
2024-07-20 12:45:43 -0400

    Added some comments

--- src/AsperandLabs.Kettlecorn/Config/PythonHttpConnectionConfig.cs
+++ src/AsperandLabs.Kettlecorn/Config/PythonHttpConnectionConfig.cs
@@ -2,7 +2,10 @@ namespace AsperandLabs.Kettlecorn.Config;
 
 public class PythonHttpConnectionConfig
 {
+    //Set this to the same dir as your python venv
     public string WorkingDirectory { get; set; }
+    //Set this to the path to the uvicorn/fastapi/gunicorn binary in your venv (run which uvicorn to get path from python project)
     public string RelativeStartFile { get; set; }
+    //This is the same entry point that you would pass to uvicorn
     public string EntryPoint { get; set; }
 }
